25cdae58df7524b4c0b7ef7d47f2c37f4f9eb931
landonbuell <lhb1007@wildcats.unh.edu>
2020-10-30 01:31:31 -0400

    Updates to password Fun

--- PasswordFun/PasswordFunPython/PasswordFunPython.py
+++ PasswordFun/PasswordFunPython/PasswordFunPython.py
@@ -7,17 +7,34 @@ import numpy as np
 import os
 import sys
 import zipfile
+import string
 
 if __name__ == "__main__":
 
     zipPath = "C:\\Users\\Landon\\Documents\\GitHub\\Buell-CS417-Fall2020\\Lab14\\Lab14\\archive.zip"
     txtPath = "C:\\Users\\Landon\\Documents\\GitHub\\Buell-CS417-Fall2020\\Lab14\\Lab14\\archiveTXT.txt"
+    expPath = "C:\\Users\\Landon\\Documents\\GitHub\\Buell-CS417-Fall2020\\Lab14"
 
-    fileData = np.loadtxt(txtPath,comments='b"',encoding='utf-8')
+    data = np.fromfile(txtPath,dtype=int,sep="")
+    
+    file = open(txtPath,mode='r',encoding='Cp437')
+    lines = file.readlines()
+    file.close()
 
+    """
+    lowerCase = list(string.ascii_lowercase)
+    upperCase = list(string.ascii_uppercase)
 
     Zip = zipfile.ZipFile(zipPath,mode='r')
-    
 
-
+    while True:
+        pwdstr = str(np.random.choice(lowerCase,size=8))
+        print(pwdstr)
+        pwd = bytes(pwdstr,encoding='Cp437')   
+        try:
+            Zip.extractall(expPath,pwd=pwd)
+            print("\t\tPWD:",pwdstr)
+        except:
+            continue
+    """
     print("=)")
--- PasswordFun/ZipFileTest/ZipFileMain.cs
+++ PasswordFun/ZipFileTest/ZipFileMain.cs
@@ -13,18 +13,7 @@ namespace ZipFileTest
             string zipPath = @"C:\Users\Landon\Documents\GitHub\Buell-CS417-Fall2020\Lab14\Lab14\archive.zip";
             string extPath = @"C:\Users\Landon\Documents\GitHub\Buell-CS417-Fall2020\Lab14";
 
-            using (FileStream zipToOpen = new FileStream(zipPath, FileMode.Open))
-            {
-                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
-                {
-                    ZipArchiveEntry readmeEntry = archive.CreateEntry("Readme.txt");
-                    using (StreamWriter writer = new StreamWriter(readmeEntry.Open()))
-                    {
-                        writer.WriteLine("Information about this package.");
-                        writer.WriteLine("========================");
-                    }
-                }
-            }
+            ZipFile.ExtractToDirectory(zipPath, extPath);
 
 
         }
