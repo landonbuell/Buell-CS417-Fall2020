64e4c982288bd3508e347e6c8dead917c9f77d23
landonbuell <lhb1007@wildcats.unh.edu>
2020-10-31 00:43:26 -0400

    ;Updated C# linked list

--- LinkedLists/LinkedListsTest/LinkedListUtils.cs
+++ LinkedLists/LinkedListsTest/LinkedListUtils.cs
@@ -1,47 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
 
 namespace LinkedListsTest
 {
     public class ListNode
     {
+        private string _val;
 
-        public string _val;
-        public ListNode _next;
-        public ListNode _prev;
 
        public ListNode(string val, ListNode next = null, ListNode prev = null)
         {
             // Constructor for ListNodeClass
             this._val = val;
-            this._next = next;
-            this._prev = prev;
+
+            this.Next = next;
+            this.Prev = prev;
+
         }
+
+        public ListNode Next { get; set; }
+
+        public ListNode Prev { get; set; }
 
     }
 
     public class DoubleLinkedList
     {
-        private ListNode _head;
 
         public DoubleLinkedList()
         {
-            // Constructor if given an exisitng linkined list
-            this._head = new ListNode(null,null,null);
+            // Constructor for DoubleLinkeList Instance
+            ListNode _head = new ListNode(null, null, null);
+            ListNode _tail = new ListNode(null, null, null);
+            this.Head = new ListNode(null, _tail, null);
+            this.Tail = new ListNode(null, null, _head); 
         }
 
-        public DoubleLinkedList(ListNode exisitngNode)
-        {
-            // Constructor if given an exisitng linkined list
-            this._head = exisitngNode;
-        }
+        public ListNode Head {get; set;}
 
-        public DoubleLinkedList (DoubleLinkedList existingList)
-        {
-            // Constructor if given an exisiting linked list
-            this._head = existingList._head;
-        }
+        public ListNode Tail { get; set; }
 
         public void PrintList()
         {
@@ -49,17 +48,6 @@ namespace LinkedListsTest
 
         }
 
-        public ListNode AddTail(ListNode newTail)
-        {
-            // Add List Node to the Tail of this Linked List
-            ListNode _currNode = _head;      //
-            while (_currNode._next != null)
-            {
-                _currNode = _currNode._next;
-            }
-            _currNode._next = newTail;
-            newTail._prev = _currNode;
-            return _head;
-        }
+       
     }
 }
